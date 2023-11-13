6850fbefea098a50ea26ee25bbb8dd1635c01baf
rneufer <90104012+rneufer@users.noreply.github.com>
2023-11-13 10:42:44 -0600

    Update HeadlessHorseman.ino
    Added initial code for horseman and pumpkin man triggers. Pumpkin will only be triggered if horseman has been triggered.

--- HeadlessHorseman/HeadlessHorseman.ino
+++ HeadlessHorseman/HeadlessHorseman.ino
@@ -1,10 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
-  // Adding this line of notes for the purpose of learning how to do branch stuff
+#include <Servo.h>
 
+Servo myServo;  // create a servo variable
+
+//setup horseman man
+const int buttonAPin = 2;
+int buttonAState = 0;
+int lastButtonAState = 0;
+bool A = false;
+
+//setup pumpkin head
+const inst buttonBPin = 3;
+int buttonBState = 0;
+int lastButtonBState = 0;
+bool B = false;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9
+  pinMode(buttonAPin, INPUT)
+  pinMode(buttonBPin, INPUT)
+  Serial.begin(9600)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonAState = digitalRead(buttonAPin);
+  
+  // Read button A (Eventually, read if cannonball is connecting circuit although
+  // i'm not exactly sure how that works yet lol)
+  if (buttonAState != lastButtonAState) {
+    if (buttonAState == HIGH) {
+      A = true;
+      myservo.write(180); // tells the servo what angle to turn 180
+    }  
+  }
+   
+  if (A == true) {
+    buttonBState = digitalRead(buttonBPin);
+    if (buttonBState != lastButtonBState) {
+      if (buttonBState == HIGH) {
+        B = true;
+      }
+    }
+  }
 
+  if (B == true)  {
+    myservo.write(90);
+}
+
+  lastButtonAState = buttonAState;
+  lastButtonBState = buttonBState;
 }
